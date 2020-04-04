c0f95aa173fe73af9b4fc905fe90f21161698e9f
gaborszekely <gabor_szekely@hotmail.com>
2020-04-04 00:44:15 -0600

    Updated

--- LC1190_reverse-substrings-between-parens.java
+++ LC1190_reverse-substrings-between-parens.java
@@ -2,38 +2,36 @@ import java.util.*;
 
 class Solution {
     public String reverseParentheses(String s) {
-        Stack<String> stack = new Stack<>();
+        Stack<String> stack = new Stack();
         stack.push("");
-
+        
         for (int i = 0; i < s.length(); ++i) {
             char token = s.charAt(i);
-
+            
             if (token == '(') {
                 stack.push("");
                 continue;
             }
-
-            int lastIndex = stack.size() - 1;
-
+            
             if (token == ')') {
                 String curr = stack.pop();
-                stack.set(lastIndex - 1, stack.get(lastIndex - 1) + reverse(curr));
+                stack.push(stack.pop() + reverse(curr));
                 continue;
             }
-
-            stack.set(lastIndex, stack.get(lastIndex) + token);
+            
+            stack.push(stack.pop() + token);
         }
-
+        
         return stack.pop();
     }
-
+    
     private String reverse(String str) {
         String res = "";
-
+        
         for (int i = 0; i < str.length(); ++i) {
             res = str.charAt(i) + res;
         }
-
+        
         return res;
     }
 }

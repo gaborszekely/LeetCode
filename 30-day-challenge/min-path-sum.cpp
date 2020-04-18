ec422eeb2f3bec5d9ee45c23a9b3a3ea6dbf6f72
gaborszekely <gabor_szekely@hotmail.com>
2020-04-18 01:26:35 -0600

    fixed formatting

--- 30-day-challenge/min-path-sum.cpp
+++ 30-day-challenge/min-path-sum.cpp
@@ -4,33 +4,32 @@ using namespace std;
 
 class Solution {
 public:
-	int minPathSum(vector<vector<int>> &grid) {
-		int r = grid.size() - 1;
-		int c = grid[0].size() - 1;
+    int minPathSum(vector<vector<int>> &grid) {
+        int r = grid.size() - 1;
+        int c = grid[0].size() - 1;
+        int minPaths[r + 1][c + 1];
 
-        int minPaths[r+1][c+1];
-
-		for (int i = r; i >= 0; --i) {
-			for (int j = c; j >= 0; --j) {
-				if (i == r && j == c) {
+        for (int i = r; i >= 0; --i) {
+            for (int j = c; j >= 0; --j) {
+                if (i == r && j == c) {
                     minPaths[i][j] = grid[i][j];
-					continue;
-				}
+                    continue;
+                }
 
-				if (i == r) {
-					minPaths[i][j] = grid[i][j] + minPaths[i][j + 1];
-					continue;
-				}
+                if (i == r) {
+                    minPaths[i][j] = grid[i][j] + minPaths[i][j + 1];
+                    continue;
+                }
 
-				if (j == c) {
-					minPaths[i][j] = grid[i][j] + minPaths[i + 1][j];
-					continue;
-				}
+                if (j == c) {
+                    minPaths[i][j] = grid[i][j] + minPaths[i + 1][j];
+                    continue;
+                }
 
-				minPaths[i][j] = grid[i][j] + min(minPaths[i][j + 1], minPaths[i + 1][j]);
-			}
-		}
+                minPaths[i][j] = grid[i][j] + min(minPaths[i][j + 1], minPaths[i + 1][j]);
+            }
+        }
 
-		return minPaths[0][0];
-	}
+        return minPaths[0][0];
+    }
 };

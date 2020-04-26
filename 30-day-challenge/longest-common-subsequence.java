83e5fc59c98c5efd7edd31aea429eb7b5bd7ff1e
gaborszekely <gabor_szekely@hotmail.com>
2020-04-26 05:00:09 -0600

    Added recursive approach

--- 30-day-challenge/longest-common-subsequence.java
+++ 30-day-challenge/longest-common-subsequence.java
@@ -1,6 +1,6 @@
 class Solution {
     /**
-     * Bottom-up using tabulation
+     * Approach #1: Bottom-up using tabulation
      */
     public int longestCommonSubsequence(final String text1, final String text2) {
         final int[][] dp = new int[text1.length() - 1][text2.length() + 1];
@@ -21,7 +21,7 @@ class Solution {
     }
 
     /**
-     * Top-down using recursion with memoization
+     * Approach #2: Top-down using recursion with memoization
      */
     public int longestCommonSubsequenceRec(final String text1, final String text2) {
         int[][] dp = new int[text1.length()][text2.length()];
@@ -51,18 +51,3 @@ class Solution {
         return dp[i1][i2];
     }
 }
-
-/**
- * // if (i1 >= text1.length || i2 >= text2.length) { // return 0; // }
- * 
- * // const serializedKey = `${i1}:${i2}`;
- * 
- * // if (!memo[serializedKey]) { // let longest = 0; // for(let i = i1; i <
- * text1.length; ++i) { // for (let j = i2; j < text2.length; ++j) { // if
- * (text1[i] === text2[j]) { // longest = Math.max(longest, 1 +
- * longestCommonSubsequence(text1, text2, i + 1, j + 1, memo)); // } // } // }
- * 
- * // memo[serializedKey] = longest; // }
- * 
- * // return memo[serializedKey];
- */

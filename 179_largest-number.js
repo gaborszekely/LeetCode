43a253e0aa34532f362f915da6d478d7fde0d794
Gabor Szekely <gaborszekely@Gabors-MacBook-Pro.local>
2022-11-25 18:09:30 -0700

    109: Largest number

---
+++ 179_largest-number.js
@@ -0,0 +1,19 @@
+/*
+https://leetcode.com/problems/largest-number/description/
+
+Given a list of non-negative integers nums, arrange them such that they form the largest number and return it.
+
+Since the result may be very large, so you need to return a string instead of an integer.
+*/
+
+/**
+ * @param {number[]} nums
+ * @return {string}
+ */
+ var largestNumber = function(nums) {
+    return nums.sort((a, b) => {
+        const aConcat = Number(`${a}${b}`);
+        const bConcat = Number(`${b}${a}`);
+        return aConcat > bConcat ? -1 : 1;
+    }).join('').replace(/^0+/, '0');
+};

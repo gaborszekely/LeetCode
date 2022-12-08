c1f54fe5afa680ad6452ad8fa1c671c07856dc21
Gabe Szekely <gabor_szekely@hotmail.com>
2022-12-07 21:11:39 -0700

    1143: Longest common subsequence

---
+++ 1143_longest-common-subsequence.js
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/longest-common-subsequence/description/
+
+/**
+ * @param {string} text1
+ * @param {string} text2
+ * @return {number}
+ */
+var longestCommonSubsequence = function (text1, text2) {
+  const dp = Array.from({ length: text1.length + 1 }, () =>
+    Array.from({ length: text2.length + 1 }, () => 0)
+  );
+
+  for (let i = 1; i < dp.length; ++i) {
+    for (let j = 1; j < dp[0].length; ++j) {
+      const char1 = text1[i - 1];
+      const char2 = text2[j - 1];
+
+      if (char1 === char2) {
+        dp[i][j] = dp[i - 1][j - 1] + 1;
+      } else {
+        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+      }
+    }
+  }
+
+  const lastRow = dp.at(-1);
+
+  return lastRow[lastRow.length - 1];
+};

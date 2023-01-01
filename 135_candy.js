704b5dcbd74ab95d84843bbd28501d1b33ea6239
Gabe Szekely <gabor_szekely@hotmail.com>
2023-01-01 16:08:50 -0700

    Refactor candy

--- 135_candy.js
+++ 135_candy.js
@@ -1,3 +1,27 @@
+// Two pass approach.
+/**
+ * @param {number[]} ratings
+ * @return {number}
+ */
+var candy = function (ratings) {
+  const candies = Array(ratings.length).fill(1);
+
+  for (let i = 1; i < ratings.length; ++i) {
+    if (ratings[i] > ratings[i - 1]) {
+      candies[i] = candies[i - 1] + 1;
+    }
+  }
+
+  for (let i = ratings.length - 2; i >= 0; --i) {
+    if (ratings[i] > ratings[i + 1]) {
+      candies[i] = Math.max(candies[i], candies[i + 1] + 1);
+    }
+  }
+
+  return sum(candies);
+};
+
+// Single pass approach.
 /**
  * @param {number[]} ratings
  * @return {number}

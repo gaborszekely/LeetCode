c27b25a4eb3a4f4971cf52885ad699b252217472
Gabe Szekely <gabor_szekely@hotmail.com>
2023-01-07 15:15:20 -0700

    Refactor 134

--- 134_gas-station.js
+++ 134_gas-station.js
@@ -7,7 +7,7 @@ var canCompleteCircuit = function (gas, cost) {
   const cache = [];
 
   for (let i = 0; i < gas.length; ++i) {
-    const canComplete = traverse(i, i, 0);
+    const canComplete = traverse(i);
     if (canComplete) {
       return i;
     }
@@ -15,28 +15,34 @@ var canCompleteCircuit = function (gas, cost) {
 
   return -1;
 
-  function traverse(index, startIndex, current) {
-    const fuel = gas[index];
-    const costToNext = cost[index];
-    const newFuel = current + fuel - costToNext;
+  function traverse(startIndex) {
+    let index = startIndex;
+    let currentFuel = 0;
 
-    if (newFuel < 0) {
-      return false;
+    while (true) {
+      const fuel = gas[index];
+      const costToNext = cost[index];
+      const newFuel = currentFuel + fuel - costToNext;
+
+      if (newFuel < 0) {
+        return false;
+      }
+
+      // Check if there was already a path to the current gas station with a higher net remaining fuel. If so, this cannot be the optimal route.
+      if (cache[index] === undefined || cache[index] < newFuel) {
+        cache[index] = newFuel;
+      } else {
+        return false;
+      }
+
+      const nextIndex = index === gas.length - 1 ? 0 : index + 1;
+
+      if (nextIndex === startIndex) {
+        return true;
+      }
+
+      currentFuel = newFuel;
+      index = nextIndex;
     }
-
-    // Check if there was already a path to the current gas station with a higher net remaining fuel. If so, this cannot be the optimal route.
-    if (cache[index] === undefined || cache[index] < newFuel) {
-      cache[index] = newFuel;
-    } else {
-      return false;
-    }
-
-    const nextIndex = index === gas.length - 1 ? 0 : index + 1;
-
-    if (nextIndex === startIndex) {
-      return true;
-    }
-
-    return traverse(nextIndex, startIndex, newFuel);
   }
 };

2e7d460ff2959bf1b93ab66ad0eda2ede2a40eac
Gabe Szekely <gabor_szekely@hotmail.com>
2023-01-29 20:02:42 -0700

    refactor 4

--- 4_median-of-two-sorted-arrays/index.js
+++ 4_median-of-two-sorted-arrays/index.js
@@ -4,8 +4,8 @@
  * @return {number}
  */
 var findMedianSortedArrays = function (nums1, nums2) {
-  if (!nums1.length) return getMedian(nums2);
-  if (!nums2.length) return getMedian(nums1);
+  if (!nums1.length) return getSingleArrayMedian(nums2);
+  if (!nums2.length) return getSingleArrayMedian(nums1);
   return (
     getPartitionedMedian(nums1, nums2) ?? getPartitionedMedian(nums2, nums1)
   );
@@ -16,39 +16,46 @@ function getPartitionedMedian(arr1, arr2) {
   let r = arr1.length - 1;
   const totalLength = arr1.length + arr2.length;
   const isEven = totalLength % 2 === 0;
-  let toLeft = Math.floor(totalLength / 2);
 
-  // Gets the median if the combined array length is even.
-  const getEvenMedian = (i, otherI) =>
-    (arr1[i] + Math.max(arr1[i - 1] ?? -Infinity, arr2[otherI] ?? -Infinity)) /
-    2;
+  // How many values should be to the left of the median value.
+  const toLeft = Math.floor(totalLength / 2);
 
   while (l <= r) {
     const mid = Math.floor((l + r) / 2);
     const midVal = arr1[mid];
-    const otherIndex = toLeft - mid - 1;
+    const arr2Index = toLeft - mid - 1;
 
-    if (otherIndex >= arr2.length) {
+    const getMedian = () =>
+      isEven
+        ? (arr1[mid] +
+            Math.max(
+              arr1[mid - 1] ?? -Infinity,
+              arr2[arr2Index] ?? -Infinity
+            )) /
+          2
+        : midVal;
+
+    if (arr2Index >= arr2.length) {
       l = mid + 1;
       continue;
     }
 
-    if (otherIndex <= -1) {
-      if (otherIndex === -1 && arr2[0] >= midVal) {
-        return isEven ? getEvenMedian(mid, otherIndex) : midVal;
+    if (arr2Index <= -1) {
+      if (arr2Index === -1 && arr2[0] >= midVal) {
+        return getMedian();
       }
       r = mid - 1;
       continue;
     }
 
     if (
-      arr2[otherIndex] <= midVal &&
-      (arr2[otherIndex + 1] ?? Infinity) >= midVal
+      arr2[arr2Index] <= midVal &&
+      (arr2[arr2Index + 1] ?? Infinity) >= midVal
     ) {
-      return isEven ? getEvenMedian(mid, otherIndex) : midVal;
+      return getMedian();
     }
 
-    if (arr2[otherIndex] > midVal) {
+    if (arr2[arr2Index] > midVal) {
       l = mid + 1;
     } else {
       r = mid - 1;
@@ -57,7 +64,7 @@ function getPartitionedMedian(arr1, arr2) {
 }
 
 // Gets the median of a single array.
-function getMedian(arr) {
+function getSingleArrayMedian(arr) {
   const mid = Math.floor(arr.length / 2);
   return arr.length % 2 === 1 ? arr[mid] : (arr[mid - 1] + arr[mid]) / 2;
 }
